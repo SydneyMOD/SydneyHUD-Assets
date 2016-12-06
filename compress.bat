5a83df4d8933237c83a31c3ada7b832210207d05
Katabame Ayame <mail@katabamia.me>
2016-12-07 00:33:02 +0900

    Revert

---
+++ compress.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d %~dp0
+
+echo deleting old file...
+del SydneyHUD-Assets.zip
+echo delete completed.
+
+echo starting compress...
+7za a SydneyHUD-Assets.zip ../SydneyHUD-Assets/ -xr!.git/ -xr!readme.md -xr!.gitignore -xr!7za.exe -xr!compress.bat
+echo compress finished.
+pause
